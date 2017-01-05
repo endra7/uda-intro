49972888d13f5db4ba9d374a4b12621faf233197
endra7 <indrapun21@gmail.com>
2017-01-05 22:47:41 +0530

    for handling no link cases and to extract all links on the page

--- link_extractor_basic.py
+++ link_extractor_basic.py
@@ -1,9 +1,33 @@
 
 page = '''<div id="top_bin"> <div id="top_content" class="width960">
    <div class="udacity float-left"> <a href="www.google.com">'''
-
+#ver 1
+'''
 start_link=page.find('<a href=')
 start_quote=page.find('"',start_link)
 end_quote=page.find('"',start_quote+1)
 url=page[start_quote+1:end_quote]
 print(url)
+'''
+
+#ver 2
+
+def get_next_target(page):
+    start_link=page.find('<a href=')
+    if(start_link != -1):
+        start_quote=page.find('"',start_link)
+        end_quote=page.find('"',start_quote+1)
+        url=page[start_quote+1:end_quote]
+        return url,end_quote
+    else:
+        return None,0
+
+def print_all_links(page):
+    while True:
+        url,endpos=get_next_target(page)
+        if url:
+            print url
+            page=page[endpos:]
+        else:
+            break
+
